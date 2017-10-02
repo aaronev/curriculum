c9a77444e1344d0df7a0fd2a3754330c2e86859f
Aaron Villanueva <aarone.villanueva@gmail.com>
2017-10-02 15:10:06 -0700

    meets requested changes

--- web-server/index.js
+++ web-server/index.js
@@ -40,10 +40,6 @@ app.get('/', (request, response, next) => {
   response.renderMarkdownFile(`/README.md`)
 })
 
-app.get('/*', (request, response, next) => {
-  response.renderFile(request.path)
-})
-
 ;[
   'CONTRIBUTING.md',
   'SUPPORT.md',
@@ -53,6 +49,9 @@ app.get('/*', (request, response, next) => {
   })
 })
 
+app.get('*', (request, response, next) => {
+  response.renderFile(request.path)
+})
 
 if (!module.parent) {
   app.listen(process.env.PORT, () => {
